02fbf38e1064e74ec49105bcf6a62674b8b8fc12
Jonathan Acra <jonathanacra@gmail.com>
2022-06-16 05:04:22 +0000

    playing around with particles.js

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Particles from "particlesjs";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  useEffect(() => {
+    Particles.init({
+      selector: ".particle_back",
+      connectParticles: "True",
+      sizeVariations: "6",
+      speed: "1",
+    });
+  });
+  const [color, setColor] = useState("");
   return (
-    <div>
-      <h1 className="text-center mt-5">Hello Rigo!</h1>
-      <p>
-        <img src={rigoImage} />
-      </p>
-      <a href="#" className="btn btn-success">
-        If you see this green button... bootstrap is working...
-      </a>
-      <p>
-        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-        love!
-      </p>
+    <div className="border border-danger d-flex mt-5 justify-content-center">
+      <canvas class="particle_back ">Hello</canvas>
+      <img
+        className="img-fluid"
+        src="https://camo.githubusercontent.com/61491d59e71fec5c794945fed916a4a682b6c0404fc31f30b08a0d919c558404/68747470733a2f2f696d616765732e73717561726573706163652d63646e2e636f6d2f636f6e74656e742f76312f3537363966633430316236333162616231616464623261622f313534313538303631313632342d5445363451474b524a4738535741495553374e532f6b6531375a77644742546f6464493870446d34386b506f73776c7a6a53564d4d2d53784f703743563539425a772d7a505067646e346a557756634a45315a7657515578776b6d794578676c4e714770304976544a5a616d574c49327a76595748384b332d735f3479737a63703272795449304871544f6161556f68724938504936465879386339505774426c7141566c555335697a7064634958445a71445976707252715a32395077306f2f636f64696e672d667265616b2e676966"
+      />
+      <script src="path/to/particles.min.js"></script>
     </div>
   );
 };
